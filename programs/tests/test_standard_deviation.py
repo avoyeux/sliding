bdcf2def02ebd56df5fec06c6a0eae3eedd0733c
Voyeux Alfred <alfredvoyeux@hotmail.com>
2026-01-24 02:30:21 +0100

    MID: Welford's formula.
    Started to test an implementation of the Welford formula, seems promising.

--- programs/tests/test_standard_deviation.py
+++ programs/tests/test_standard_deviation.py
@@ -13,6 +13,39 @@ from programs.standard_deviation import FastStandardDeviation
 # TYPE ANNOTATIONs
 import queue
 
+import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+
+def sliding(arr: np.ndarray, kernel_shape: tuple[int, ...]):
+    # todo add docstring
+
+    axis = tuple(range(arr.ndim))
+    assert len(kernel_shape) == len(axis), "kernel_shape match number of axes"
+    
+    pad = tuple((k // 2, k // 2) for k in kernel_shape)
+    arr = TestUtils.add_padding(
+        border='reflect',
+        data=arr,
+        pad=pad,
+    )
+
+    # VIEW
+    windows = sliding_window_view(
+        x=arr,
+        window_shape=kernel_shape,
+        axis=axis,#type: ignore
+    )
+
+    kernel_axes = tuple(range(-len(axis), 0))
+
+    # MEAN
+    mean = windows.mean(axis=kernel_axes)
+
+    # STD stable
+    diff = windows - np.expand_dims(mean, axis=kernel_axes)
+    M2 = np.sum(diff**2, axis=kernel_axes)
+    std = np.sqrt(M2 / np.prod(kernel_shape))
+    return mean, std
 
 
 class TestStandardDeviation:
@@ -68,12 +101,12 @@ class TestStandardDeviation:
 
             kernel = (3,) * data.ndim
 
-            # OLD std
-            old_standard_deviation = TestUtils.old_implementation(
-                function='std',
-                data=data,
-                kernel_size=kernel,
-            )
+            # # OLD std
+            # old_standard_deviation = TestUtils.old_implementation(
+            #     function='std',
+            #     data=data,
+            #     kernel_size=kernel,
+            # )
 
             # NEW std
             new_standard_deviation = FastStandardDeviation(
@@ -83,13 +116,19 @@ class TestStandardDeviation:
                 with_NaNs=True,
                 threads=1,
             ).sdev
+            # _, new_standard_deviation = sliding_welford(
+            #     arr=data,
+            #     kernel_shape=kernel,
+            #     circular=False,
+            #     axis=None,
+            # )
 
             # print(f"old and new dtypes are {old_standard_deviation.dtype} and {new_standard_deviation.dtype}", flush=True)
 
-            # COMPARISON
-            comparison_log = TestUtils.compare(
-                actual=new_standard_deviation,
-                desired=old_standard_deviation,
-                filepath=filepath,
-            )
-            result_queue.put(comparison_log)
+            # # COMPARISON
+            # comparison_log = TestUtils.compare(
+            #     actual=new_standard_deviation,
+            #     desired=old_standard_deviation,
+            #     filepath=filepath,
+            # )
+            # result_queue.put(comparison_log)

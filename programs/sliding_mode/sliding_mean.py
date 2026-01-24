b09094c58c684e93b3efc3efbe7e35de1f153ca6
Voyeux Alfred <alfredbvoyeux@hotmail.com>
2026-01-24 13:17:33 +0100

    LOW: sliding mean type annotation and docstring update.
    I changed the type annotations for the new sliding mean class to an older version that I understood more. More likely, is going to be the last type annotation version for that code even though there is no static type constraints on the dtype of the input arrays. Furthemore, I updated all the docstrings inside that same class.

--- programs/sliding_mode/sliding_mean.py
+++ programs/sliding_mode/sliding_mean.py
@@ -11,50 +11,50 @@ import numpy.typing as npt
 from programs.standard_deviation import BorderType, Convolution  # ! need to change the path to this code
 
 # TYPE ANNOTATIONs
-from typing import cast, Any, overload, Protocol, TypeVar
+from typing import cast
 type Array[D: np.floating] = npt.NDArray[D]
-D_co = TypeVar("D_co", bound=np.floating, covariant=True)
-D_inv = TypeVar("D_inv", bound=np.floating, covariant=False)
-class _Single(Protocol[D_co]): _mean: npt.NDArray[D_co]
-class _Multi(Protocol[D_inv]): _mean: list[npt.NDArray[D_inv]]
+type ArrayLike[D: np.floating] = Array[D] | list[Array[D]]
 
 # API public
 __all__ = ["SlidingMean"]
 
 
 
-class SlidingMean[D: np.floating[Any]]:
-
-    @overload
-    def __init__(
-        self,
-        data: Array[D],
-        kernel: Array[D],
-        borders: BorderType = "reflect",
-        threads: int = 1,
-    ) -> None: ...
-
-    @overload
-    def __init__(
-        self,
-        data: list[Array[D]],
-        kernel: Array[D],
-        borders: BorderType = "reflect",
-        threads: int = 1,
-    ) -> None: ...
+class SlidingMean[Data: ArrayLike]:
+    """
+    To compute the sliding mean of a given ndarray data and kernel.
+    The inputs need to be of float32 or float64 type.
+    """
 
     def __init__(
         self,
-        data: Array[D] | list[Array[D]],
-        kernel: Array[D],
+        data: Data,
+        kernel: Array,
         borders: BorderType = "reflect",
         threads: int = 1,
     ) -> None:
-        # todo add docstring
+        """
+        Computes the sliding mean of a given ndarray data and kernel.
+        Weights can be used inside the kernel and the input data can have np.nan values.
+        To access the sliding mean result, use the `sliding_mean` property.
+        When the input data is given as a list of arrays, is it supposed that the positions of
+        np.nan are the same for all arrays. Furthermore, the kernel used remains the same for all
+        arrays.
+        # ! make sure that the kernel and data have the same dtype to avoid unwanted behaviors.
 
-        # ? do i need to normalize the kernel, don't think so no ?
+        Args:
+            data (Data): the input data (or list of data) to compute the sliding mean from.
+            kernel (Array): the kernel to use for the sliding mean computation. Can contain
+                weights.
+            borders (BorderType, optional): the type of borders to use. These are the type of
+                borders used by OpenCV (not all OpenCV borders are implemented as some don't
+                have the equivalent in np.pad or scipy.ndimage). If None, uses adaptative borders,
+                i.e. no padding and hence smaller kernels at the borders. Defaults to 'reflect'.
+            threads (int | None, optional): the number of threads to use for the computation.
+                If None, doesn't change change the default behaviour. Defaults to 1.
+        """
 
-        self._data = cast(list[Array[D]], data if isinstance(data, list) else [data])
+        self._data = cast(list[Array], data if isinstance(data, list) else [data])
         self._kernel = kernel
         self._borders = borders
         self._threads = threads
@@ -62,20 +62,31 @@ class SlidingMean[D: np.floating[Any]]:
         # COMPUTE
         self._mean = self._sliding_mean()
 
-    @overload
-    def sliding_mean(self: _Single[D]) -> Array[D]: ...
+    @property
+    def sliding_mean(self) -> Data:
+        """
+        Gives the sliding mean results.
+        Keep in mind that, if the input data was a list of arrays, then the results is also a list
+        of array with the same type and shape than the input data. Please do make sure that the
+        kernel and input data have the same dtype to not create unwanted behaviors.
 
-    @overload
-    def sliding_mean(self: _Multi[D]) -> list[Array[D]]: ...
+        Returns:
+            Data: _description_
+        """
 
-    def sliding_mean(self) -> Array[D] | list[Array[D]]:
-        # todo add docstring
-
-        if len(self._mean) == 1: return self._mean[0]
+        if len(self._mean) == 1: return cast(Data, self._mean[0])
         return self._mean
 
-    def _get_not_nans_count(self) -> tuple[np.ndarray[tuple[int, ...], np.dtype[np.bool_]], Array[D]]:
-        # todo add docstring
+    def _get_not_NaNs_count(self) -> tuple[np.ndarray[tuple[int, ...], np.dtype[np.bool_]], Array]:
+        """
+        To get the mask of non nan values and the count of nan values for each sliding window.
+        Done so that the nan values can be swapped with 0. The result of the sliding mean is then
+        corrected with this count.
+
+        Returns:
+            tuple[np.ndarray[tuple[int, ...], np.dtype[np.bool_]], Array]: the mask of valid data
+                and the sliding window count of nan values.
+        """
 
         # ! if I normalise the kernel, no need for this if there is no NaN
 
@@ -83,7 +94,7 @@ class SlidingMean[D: np.floating[Any]]:
         self._borders = cast(BorderType, self._borders)
 
         # NaN handling
-        valid_mask = ~np.isnan(self._data[0])  # even if no Nans need it for border effects
+        valid_mask = ~np.isnan(self._data[0])  # even if no NaNs need it for border effects
 
         # COUNT NaN
         count = Convolution(
@@ -94,17 +105,23 @@ class SlidingMean[D: np.floating[Any]]:
         ).result
         return valid_mask, count
 
-    def _sliding_mean(self) -> Array[D] | list[Array[D]]:
-        # todo add docstring
+    def _sliding_mean(self) -> Data:
+        """
+        Computes the sliding mean.
+
+        Returns:
+            Data: the sliding mean values as an array of np.ndarray. The length of the list is 1
+                if the input data was a single array.
+        """
 
         # TYPE CHECKER complains
         self._borders = cast(BorderType, self._borders)
 
         # NaN handling
-        nan_mask, count = self._get_not_nans_count()
+        nan_mask, count = self._get_not_NaNs_count()
 
         # MEMORY pre-allocation
-        means: list[Array[D]] = cast(list[Array[D]], [None] * len(self._data))
+        means: Data = cast(Data, [None] * len(self._data))
 
         for i, data in enumerate(self._data):
 
@@ -124,7 +141,7 @@ class SlidingMean[D: np.floating[Any]]:
             with np.errstate(divide='ignore', invalid='ignore'):
                 means[i] = np.where(count > 0, sum_values / count, 0.0).astype(data.dtype)
         return means
-    
+
 
 
 if __name__ == "__main__":
@@ -135,4 +152,4 @@ if __name__ == "__main__":
         borders='reflect',
         threads=1,
     )
-    mean = instance.sliding_mean()
+    mean = instance.sliding_mean
